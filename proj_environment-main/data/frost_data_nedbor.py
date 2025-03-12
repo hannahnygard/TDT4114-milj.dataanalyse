22a39d94b838541d45cfb6f7a83ef251b881ad97
idacb <icbakke@stud.ntnu.no>
2025-03-12 12:50:21 +0100

    Hente JSONdata fra Frost

---
+++ proj_environment-main/data/frost_data_nedbor.py
@@ -0,0 +1,42 @@
+import requests
+import os 
+from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth
+import json
+
+
+#laster opp variabler fra .env-fil
+dotenv_path = "api_nokkel.env"
+load_dotenv(dotenv_path=dotenv_path) 
+
+#henter ut ut variabler fra .env-fil
+api_key = os.getenv("API_KEY")
+
+URL = "https://frost.met.no/observations/v0.jsonld"
+by_oslo = "SN18700"
+
+
+# Parametere for forespørselen
+parametere = {
+
+    "sources": by_oslo,
+    "referencetime": "1980-01-01/2020-12-31",
+    "elements": "accumulated(precipitation_amount)"
+
+}
+ 
+# Send request
+response = requests.get(URL, params=parametere, auth=HTTPBasicAuth(api_key, ""))
+
+if response.status_code == 200:
+    data = response.json()
+    
+else:
+    print("feil:", response.status_code, response.text)
+
+
+#Filsti for JSON-filen
+filsti = os.path.join('data', 'frost_nedbor.json')
+#Oppretter JSON-fil med dataen - lukker filen automatisk 
+with open(filsti, 'w') as json_file:
+    json.dump(data, json_file, indent=4)
--- proj_environment-main/data/frost_data_temp.py
+++ proj_environment-main/data/frost_data_temp.py
@@ -13,17 +13,14 @@ load_dotenv(dotenv_path=dotenv_path)
 api_key = os.getenv("API_KEY")
 
 URL = "https://frost.met.no/observations/v0.jsonld"
-
 by_oslo = "SN18700"
 
-# Parametere for forespørselen
 
+# Parametere for forespørselen
 parametere = {
 
     "sources": by_oslo,
-
-    "referencetime": "1980-01-01/2020-02-01",
-
+    "referencetime": "1980-01-01/2020-12-31",
     "elements": "best_estimate_mean(air_temperature P1Y)"
 
 }
@@ -34,13 +31,12 @@ response = requests.get(URL, params=parametere, auth=HTTPBasicAuth(api_key, ""))
 if response.status_code == 200:
     data = response.json()
     
-
 else:
     print("feil:", response.status_code, response.text)
 
 
 #Filsti for JSON-filen
-filsti = os.path.join('data', 'frost.json')
-#Oppretter JSON-fil med dataen - closer filen automatisk 
+filsti = os.path.join('data', 'frost_temp.json')
+#Oppretter JSON-fil med dataen - lukker filen automatisk 
 with open(filsti, 'w') as json_file:
     json.dump(data, json_file, indent=4)
