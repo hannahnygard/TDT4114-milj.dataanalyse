d27a2300b00f6e12058b800ad0ffb4b22a42dc1c
idacb <icbakke@stud.ntnu.no>
2025-04-04 12:55:40 +0200

    Videreutvikling av dataRens-klassen, la inn nye_nedbør_verdier()

--- proj_environment-main/src/data_rens.py
+++ proj_environment-main/src/data_rens.py
@@ -68,58 +68,30 @@ class DataRens:
 
         return df
 
-    '''
-    def rens_DataFrame(self, df):
-        #Leser dataene etter dato, før vi deretter beregner gjennomsnittet
-        #df_nedbor_avg = df.groupby("referenceTime", as_index=False)["value"].mean()
-        #df_nedbor_avg["unit"] = df.groupby("referenceTime")["unit"].first().reset_index(drop=True)
-        
-
-        #Telle duplikater før de fjernes
-        def tell_duplikater():
-            antall_duplikater = df.duplicated().sum()
-            retur_av_duplikater = print(f"Antall duplikater funnet: {antall_duplikater}")
-
-            #Dersom dataen har duplikater, blir de fjernet
-            if antall_duplikater > 0:
-                df.drop_duplicates(inplace=True)
-            
-            return retur_av_duplikater
-
-        def finn_år_som_mangler_verdi():
-            #Skjekker om det er noen år som mangler en verdi
-            manglende_år = df["value"].isna().sum()
-            if manglende_år > 0:
-                print(f"Antall år med manglende verdi: {manglende_år}")
-                print("År uten verdi:")
-                print(df[df["value"].isna()])
-            else:
-                print("Det er ingen datoer som mangler verdier!")
-
-        #Endrer "referencetime" til "year"
-        df["year"] = pd.to_datetime(df["referenceTime"]).dt.year
-
-        #Beholder kun de kolonnene jeg vil ha 
-        df = df[["year", "value", "unit"]]
-
-        return df
-    '''
-
-
     def nye_nedbør_verdier(self, df):
-        #Sum per år
-        df_nedbor_avg = df.groupby("year", as_index=False)["value"].sum()
-        
-        #Funksjon for å sjekke om et år er skuddår
+        #Lager en kopi av dataframen for å unngå feilmeldingen SettingsWithCopyWarning 
+        df = df.copy()
+
+        #Funksjon for å sjekke om det er skuddår
         def er_skuddår(år):
             return (år % 4 == 0 and år % 100 != 0) or (år % 400 == 0)
-        
-        #Beregn gjennomsnitt per dag basert på om året er skuddår
-        df_nedbor_avg["days"] = df_nedbor_avg["year"].apply(lambda x: 366 if er_skuddår(x) else 365)
-        df_nedbor_avg["avg_per_day"] = (df_nedbor_avg["value"] / df_nedbor_avg["days"]).round(2)
-        
-        #Legger til unit etter avg_per_day
-        df_nedbor_avg["unit"] = df_nedbor_avg["unit"].dropna().unique()[0] 
 
-        return df_nedbor_avg
+        # Legg til kolonnen 'days'
+        df["days"] = df["year"].apply(lambda x: 366 if er_skuddår(x) else 365)
+
+        # Legg til kolonnen 'avg_per_day'
+        df["avg_per_day"] = (df["value"] / df["days"]).round(2)
+
+        #Endrer til den rekkefølgen vi vil ha
+        df = df[["year", "value", "avg_per_day", "unit", "days"]]
+
+        return df
+
+
+
+
+
+
+
+
     
