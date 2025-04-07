86cc7ceb9f41327a650ce61b0c8fccb22d89a914
idacb <icbakke@stud.ntnu.no>
2025-04-07 09:32:34 +0200

    Opprettet ny klasse for Datamanipulering

--- proj_environment-main/src/data_rens.py
+++ proj_environment-main/src/data_rens.py
@@ -32,7 +32,6 @@ class DataRens:
                     'timeOffset': observation.get('timeOffset', None),
                     'performanceCategory': observation.get('performanceCategory', None)
                 })
-    
 
         #Lag en Pandas DataFrame
         df = pd.DataFrame(rader)
@@ -48,10 +47,10 @@ class DataRens:
         df = df[["year", "value", "unit"]]
 
         # Fjern verdi for et tilfeldig år
-        df = self.fjern_verdi_for_tilfeldig_aar(df)  #TEST
+        #df = self.fjern_verdi_for_tilfeldig_aar(df)  #TEST
 
         # Legg til duplikat for et tilfeldig år
-        df = self.legg_til_duplikater_for_tilfeldig_aar(df)  #TEST
+        #df = self.legg_til_duplikater_for_tilfeldig_aar(df)  #TEST
 
         # Telle duplikater før de fjernes
         antall_duplikater = df.duplicated(subset=["year"]).sum()
@@ -74,45 +73,7 @@ class DataRens:
 
 
         return df
-    
 
-
-
-    def fjern_verdi_for_tilfeldig_aar(self, df):                #MANIPULERING
-        # Velger et tilfeldig år mellom 1980 og 2020
-        aar_for_fjerning = random.choice(range(1980, 2021))
-        
-        # Hent radene for dette året
-        rader_for_aar = df[df["year"] == aar_for_fjerning]
-        
-        if not rader_for_aar.empty:
-            # Velger en tilfeldig rad fra dette året og setter verdien til None
-            tilfeldig_rad = rader_for_aar.sample(n=1)
-            df.loc[tilfeldig_rad.index, "value"] = None
-            print(f"Fjern verdi for år {aar_for_fjerning} (rad: {tilfeldig_rad.index[0]})")
-        else:
-            print(f"Fant ikke data for år {aar_for_fjerning}")
-
-        return df
-
-
-
-    def legg_til_duplikater_for_tilfeldig_aar(self, df):        #MANIPULERING
-        # Velger et tilfeldig år mellom 1980 og 2020
-        aar_for_duplikat = random.choice(range(1980, 2021))
-        
-        # Hent radene for dette året
-        rader_for_aar = df[df["year"] == aar_for_duplikat]
-        
-        if not rader_for_aar.empty:
-            # Dupliserer radene for dette året
-            duplikater = rader_for_aar.sample(n=1, replace=True)  # Lager duplikater
-            df = pd.concat([df, duplikater], ignore_index=True)
-            print(f"Legger til duplikat for år {aar_for_duplikat}")
-        else:
-            print(f"Fant ikke data for år {aar_for_duplikat}")
-        
-        return df
 
 
 
