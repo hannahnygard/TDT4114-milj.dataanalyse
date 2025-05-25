ee0ab4d43f076defa5d39d85409905fa563ffd0c
idacb <icbakke@stud.ntnu.no>
2025-05-25 18:01:03 +0200

    Funksjonsdokumentasjon for prediktiv_analyse

--- proj_environment-main/src/prediktive_analyser.py
+++ proj_environment-main/src/prediktive_analyser.py
@@ -5,7 +5,17 @@ from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 
 class PrediktivModell:
-    def __init__(self, df: pd.DataFrame, input_col: str, target_col: str):
+    def __init__(self, df, input_col, target_col):
+        '''
+        Vi inisialiserer den lineære modellen
+
+        Parametere:
+            df: dataframe som inneholder både input- og målvariabel
+            input_col: navnet på input-kolonnen
+            target_com: navnet på mål-kolonnen
+
+        '''
+
         self.df = df
         self.input_col = input_col
         self.target_col = target_col
@@ -15,15 +25,30 @@ class PrediktivModell:
         self.trained = False
 
     def del_data(self, test_size=0.2, random_state=42):
+        '''
+        Deler datasettet inn i trenings- og testsett med train_test_split
+        '''
+
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             self.X, self.y, test_size=test_size, random_state=random_state
         )
 
     def tren_modell(self):
+        '''
+        Trene modellen på treningsdataen (x-train og y-train)
+        setter trained = True når modellen er trent
+        '''
         self.model.fit(self.X_train, self.y_train)
         self.trained = True
 
     def evaluer(self):
+        '''
+        Evaluerer modellen basert på x_test og y_test
+        
+        Exception reises dersom modellen ikke er trent (Dersom trained != True fra over)
+        
+        Returnere en orbok med MSE-score og R2-score
+        '''
         if not self.trained:
             raise Exception("Modellen er ikke trent ennå.")
         y_pred = self.model.predict(self.X_test)
@@ -32,12 +57,23 @@ class PrediktivModell:
         return {"MSE": mse, "R2": r2}
 
     def prediker(self, framtidige_verdier: list):
+        '''
+        Tar inn en liste med fremtidige input verdier (år)
+        Bruker den trente modellen til å forutsi målverdiene
+        '''
+        
         if not self.trained:
             raise Exception("Modellen er ikke trent ennå.")
         framtidige_df = pd.DataFrame({self.input_col: framtidige_verdier})
         return self.model.predict(framtidige_df)
 
     def plott(self):
+        '''
+        Lager et plott med treningsdata, testdata og modellens predikerte linje
+        Brukes for å visuelt evaluere modellen
+        
+        Reiser en Exception dersom modellen ikke er trent
+        '''
         if not self.trained:
             raise Exception("Modellen er ikke trent ennå.")
         
@@ -58,6 +94,13 @@ class PrediktivModell:
 
 
     def visualiser_prediksjon_med_test(self, framtidige_verdier: list):
+        '''
+        Viser samme graf som plott(), men legger til de predikterte verdiene 
+        
+        Tar inn en liste med de fremtidige verdien som parameter
+        
+        Reiser Exception dersom modellen ikke er trent 
+        '''
         if not self.trained:
             raise Exception("Modellen er ikke trent ennå.")
 
@@ -84,18 +127,11 @@ class PrediktivModell:
     @staticmethod
     def plotte_prediksjoner_samme_plot(framtidige_år, prediksjoner_dict):
         """
-        Plott flere sett med prediksjoner i samme graf.
+        Plotter flere sett med prediksjoner i samme graf.
 
-        Parameters:
-        - framtidige_år: liste med årstall (f.eks. [2021, 2022, ...])
-        - prediksjoner_dict: dict med format {variabelnavn: np.array med prediksjoner}
-
-        Eksempel:
-        prediksjoner_dict = {
-            "Temperatur": temp_prediksjoner,
-            "Nedbør": nedbor_prediksjoner,
-            "Skydekke": skydekke_prediksjoner
-        }
+        Parametere:
+        - framtidige_år: liste med årstall
+        - prediksjoner_dict: dict med predikasjonene som er funnet
         """
         plt.figure(figsize=(15, 7))
 
