4235cfa6aa559ee0c777e5414550eeca10e51fbf
idacb <icbakke@stud.ntnu.no>
2025-05-26 10:32:14 +0200

    Ferdigstilling av widget, for nedbør og temp

--- proj_environment-main/src/data_rens_widget.py
+++ proj_environment-main/src/data_rens_widget.py
@@ -3,6 +3,13 @@ from IPython.display import clear_output, display
 
 class DataRensWidget:
     def __init__(self, data_rens_instans, df_urenset):
+        '''
+        Oppstart av objekt i klassen
+
+        Parametere:
+        -En renset dataframe, instand fra klassen DataRens
+        -En urenset (manipulert) dataframe, instans fra klassen DataManipulering
+        '''
         self.data_rens = data_rens_instans
         self.df_urenset = df_urenset
         
@@ -12,7 +19,7 @@ class DataRensWidget:
                 ('Median', 'median'), 
                 ('Interpolering', 'interpolate')
                 ],
-            value='mean',
+            value='mean', #setter gjennomsnitt som default
             description='Fyll manglende med:',
         )
 
@@ -20,13 +27,22 @@ class DataRensWidget:
         self.kjør_knapp = widgets.Button(description="Rens data")
         self.output = widgets.Output()
 
-        # Sørg for at knappen ikke dobbelkobles
-        if not hasattr(self.kjør_knapp, '_click_callback_added'):
-            self.kjør_knapp.on_click(self.kjør_rensing)
-            self.kjør_knapp._click_callback_added = True
+         # Bruk on_click, men sørg for å ikke koble opp flere ganger
+        self.kjør_knapp.on_click(self.kjør_rensing)
+
+         # Intern flagg for å holde styr på om rensing allerede er gjort
+        self.already_clicked = False
+
+
 
 
     def kjør_rensing(self, _):
+        '''
+        Renser data i dataframe ved å fylle ut mnglende verdi med valgt valg
+        Viser resultat i en jupyter widget
+        
+        Parametere: ingen
+        '''
         with self.output:
             clear_output(wait=True)  # Tøm ut tidligere output
             df = self.df_urenset.copy()
@@ -35,11 +51,11 @@ class DataRensWidget:
             if metode == 'mean':
                 fyll_verdi = df['value'].mean()
                 df['value'] = df['value'].fillna(fyll_verdi)
-                print("Erstattet manglende verdier med gjennomsnitt")
+                print("Erstattet manglende verdier med gjennomsnitt", fyll_verdi)
             elif metode == 'median':
                 fyll_verdi = df['value'].median()
                 df['value'] = df['value'].fillna(fyll_verdi)
-                print("Erstattet manglende verdier med median")
+                print("Erstattet manglende verdier med median", fyll_verdi)
             elif metode == 'interpolate':
                 df['value'] = df['value'].interpolate()
                 print("Erstattet manglende verdier med interpolering")
@@ -48,5 +64,9 @@ class DataRensWidget:
             display(df.head())
 
     def vis(self):
-        clear_output(wait=True)  # NY LINJE
+        '''
+        Viser brukergrensesnittet for datarensing 
+        Returnerer None, kjører en display
+        '''
+        clear_output(wait=True)  
         display(self.metode_dropdown, self.kjør_knapp, self.output)
